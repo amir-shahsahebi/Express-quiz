b8301a77d2ed4ac8058b5c91dea0deaaf35b87bc
amir shahsahebi <shahsahebiamir@gmail.com>
2022-04-21 01:16:21 +0430

    quiz2 completed

--- index.js
+++ index.js
@@ -7,6 +7,13 @@ server.get("/chocolate", (req,res) => {
     res.send(`<h1>You want ${amount} chocolate</h1>`)
 })
 
+server.get("/multiply", (req, res) => { 
+    let {value1 , value2} = req.query;
+    let equal = value1 * value2;
+    // console.log(value1,value2,equal);
+    res.send(`the multiply of ${value1} & ${value2} is equal to ${equal}`)
+})
+
 server.listen(PORT, () => {
     console.log(`server is runing on port ${PORT}`);
 })
