213b96a410e36f861560de528b1df9a1183a165f
amir shahsahebi <shahsahebiamir@gmail.com>
2022-04-21 01:06:11 +0430

    quiz 1 completed

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const server = express();
-const PORT = 8685
-server.listen(PORT, () => {
-    console.log(`server is runing on port ${port}`);
+const PORT = 8685;
+
+server.get("/chocolate", (req,res) => {
+    let {amount} = req.query;
+    res.send(`<h1>You want ${amount} chocolate</h1>`)
 })
+
+server.listen(PORT, () => {
+    console.log(`server is runing on port ${PORT}`);
+})
+
+// link for test: http://localhost:8685/chocolate?amount=100
